488445b132b6e52123cd36658f42cb7268eb990a
Timofey Krivenkov <drake9100@mail.ru>
2023-04-10 23:47:34 +0300

    UPD: Поменял местами типы записей

--- functions/weddings/registrations.php
+++ functions/weddings/registrations.php
@@ -2,35 +2,35 @@
 
 // Регистрируем
 add_action( 'init', function () {
-	register_post_type( 'events', [
+	register_post_type( 'weddings', [
 		'labels'           => [
-			'name'               => 'Мероприятия',
-			'singular_name'      => 'Мероприятия',
-			'name_admin_bar'     => 'Мероприятия',
-			'menu_name'          => 'Мероприятия',
+			'name'               => 'Свадьбы',
+			'singular_name'      => 'Свадьбы',
+			'name_admin_bar'     => 'Свадьбы',
+			'menu_name'          => 'Свадьбы',
 			'add_new'            => 'Добавить новое',
-			'add_new_item'       => 'Добавить новую мероприятие',
-			'edit_item'          => 'Редактировать мероприятие',
-			'new_item'           => 'Новое мероприятие',
-			'view_item'          => 'Посмотреть мероприятие',
-			'search_items'       => 'Найти мероприятие',
-			'not_found'          => 'Мероприятие не найдено',
-			'not_found_in_trash' => 'В корзине мероприятий не найдено',
+			'add_new_item'       => 'Добавить новую свадьбу',
+			'edit_item'          => 'Редактировать свадьбу',
+			'new_item'           => 'Новая свадьба',
+			'view_item'          => 'Посмотреть свадьбу',
+			'search_items'       => 'Найти свадьбу',
+			'not_found'          => 'Свадьба не найдено',
+			'not_found_in_trash' => 'В корзине свадеб не найдено',
 			'title_placeholder'  => 'Введите название пакета',
 		],
 		'public'           => true,
 		'rewrite'          => true,
 		'capability_type'  => 'post',
-		'has_archive'      => 'events',
+		'has_archive'      => 'weddings',
 		'hierarchical'     => false,
-		'menu_position'    => 9,
+		'menu_position'    => 8,
 		'menu_icon'        => 'dashicons-megaphone',
 		'supports'         => [ 'title', 'thumbnail', 'editor' ],
-		'posts_per_page'   => -1,
+		'posts_per_page'   => - 1,
 		'show_in_rest'     => true,
 		'show_ui'          => true,
 		'show_in_menu'     => true,
-		'template_archive' => '/templates/events-page/events-page.php',
+		'template_archive' => '/templates/weddings-page/weddings-page.php',
 	] );
 
 }, 9 );
