2f2f66d3f6cc431082f306e4861b8fd0fc697ffa
Timofey Krivenkov <drake9100@mail.ru>
2023-04-11 12:38:46 +0300

    ADD: Шаблоны страниц

--- functions/other/page-templates.php
+++ functions/other/page-templates.php
@@ -14,6 +14,8 @@ function add_templates_to_dropdown( $templates ) {
 	// выбор шаблона в атрибутах
 	$templates['templates/about-page/about-page.php'] = 'Шаблон страницы О нас';
 	$templates['templates/contacts-page/contacts-page.php'] = 'Шаблон страницы Контакты';
+	$templates['templates/conditions-page/conditions-page.php'] = 'Шаблон страницы Условия работы';
+	$templates['templates/events-page/events-page.php'] = 'Шаблон страницы Организация мероприятий под ключ';
 
 	return $templates;
 }
